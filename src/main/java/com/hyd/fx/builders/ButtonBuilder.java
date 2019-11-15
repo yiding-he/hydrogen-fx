9dd3aed3facf5e7294f7cfe6d0f95900907b5fab
He Yiding <yiding.he@gmail.com>
2019-11-15 16:27:14 +0800

    Make button size fixed

--- src/main/java/com/hyd/fx/builders/ButtonBuilder.java
+++ src/main/java/com/hyd/fx/builders/ButtonBuilder.java
@@ -2,6 +2,7 @@ package com.hyd.fx.builders;
 
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
 
 /**
  * @author SYSTEM
@@ -11,12 +12,14 @@ public class ButtonBuilder {
     public static Button iconButton(String text, Node graph, Runnable onAction) {
         Button button = new Button(text, graph);
         button.setOnAction(event -> onAction.run());
+        button.setMinWidth(Region.USE_PREF_SIZE);
         return button;
     }
 
     public static Button button(String text, Runnable onAction) {
         Button button = new Button(text);
         button.setOnAction(event -> onAction.run());
+        button.setMinWidth(Region.USE_PREF_SIZE);
         return button;
     }
 }
