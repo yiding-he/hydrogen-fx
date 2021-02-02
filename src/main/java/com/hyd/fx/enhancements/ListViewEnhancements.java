86948728bf8dcb4f44a7f054c89fa5b5caf3f653
Yiding He <yiding.he@gmail.com>
2021-02-02 18:31:24 +0800

    Update ListViewEnhancements

--- src/main/java/com/hyd/fx/enhancements/ListViewEnhancements.java
+++ src/main/java/com/hyd/fx/enhancements/ListViewEnhancements.java
@@ -1,4 +1,13 @@
 package com.hyd.fx.enhancements;
 
+import javafx.scene.control.ListView;
+
+import java.util.function.Consumer;
+
 public interface ListViewEnhancements {
+
+    static <T> void onSelectionChanged(ListView<T> listView, Consumer<T> tConsumer) {
+        listView.getSelectionModel().selectedItemProperty()
+            .addListener((observable, oldValue, newValue) -> tConsumer.accept(newValue));
+    }
 }
