29b6e442bcce2d3774b343afee32b6afb9aaf53a
Yiding He <yiding.he@gmail.com>
2022-03-03 13:24:44 +0800

    update to java11

--- src/main/java/com/hyd/fx/builders/IconBuilder.java
+++ src/main/java/com/hyd/fx/builders/IconBuilder.java
@@ -1,24 +1,11 @@
 package com.hyd.fx.builders;
 
 import com.hyd.fx.FxException;
-import de.jensd.fx.glyphs.GlyphIcons;
-import de.jensd.fx.glyphs.GlyphsDude;
-import java.net.URL;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
+
+import java.net.URL;
 
 public class IconBuilder {
-
-    public static Text icon(GlyphIcons icon, String color) {
-        return icon(icon, "16px", color);
-    }
-
-    public static Text icon(GlyphIcons icon, String size, String color) {
-        Text text = GlyphsDude.createIcon(icon, size);
-        text.setFill(Color.web(color));
-        return text;
-    }
 
     public static Image icon(String resourcePath) {
         URL resource = LayoutBuilder.class.getResource(resourcePath);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,20 @@ module com.hyd.fx {
     requires javafx.fxml;
     requires org.slf4j;
     requires java.prefs;
-    requires fontawesomefx;
     requires org.apache.commons.io;
 
     opens com.hyd.fx to javafx.fxml;
     exports com.hyd.fx;
+    exports com.hyd.fx.app;
+    exports com.hyd.fx.builders;
+    exports com.hyd.fx.cells;
+    exports com.hyd.fx.components;
+    exports com.hyd.fx.concurrency;
+    exports com.hyd.fx.dialog;
+    exports com.hyd.fx.dialog.form;
+    exports com.hyd.fx.enhancements;
+    exports com.hyd.fx.helpers;
+    exports com.hyd.fx.style;
+    exports com.hyd.fx.system;
+    exports com.hyd.fx.utils;
 }
