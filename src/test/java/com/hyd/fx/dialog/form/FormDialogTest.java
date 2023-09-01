2f66845d71d84734eea828323c78a735611c1e7a
Yiding He <yiding.he@gmail.com>
2023-09-01 11:04:57 +0800

    Update unit tests

--- src/test/java/com/hyd/fx/dialog/form/FormDialogTest.java
+++ src/test/java/com/hyd/fx/dialog/form/FormDialogTest.java
@@ -8,53 +8,62 @@ import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class FormDialogTest extends Application {
+public class FormDialogTest {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(FormDialogTestApp.class, args);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        AppPrimaryStage.setPrimaryStage(primaryStage);
-
-        primaryStage.setScene(new Scene(LayoutBuilder.vbox(50, 0,
-            ButtonBuilder.button("打开对话框", this::openDialog))));
-
-        primaryStage.show();
-    }
-
-    private void openDialog() {
-        MyFormDialog myFormDialog = new MyFormDialog(AppPrimaryStage.getPrimaryStage());
-        myFormDialog.setTitle("测试表单");
-        myFormDialog.setWidth(500);
-        myFormDialog.setHeight(400);
-        myFormDialog.show();
-    }
-
-    //////////////////////////////////////////////////////////////
-
-    public static class MyFormDialog extends FormDialog {
-
-        public MyFormDialog(Stage owner) {
-            super(owner);
-            addField(new TextFormField().label("名字").text("1231231").validation(f -> {
-                if (f.getText().isEmpty()) {
-                    throw new RuntimeException("名字不能为空");
-                }
-            }));
-            addField(new TextAreaFormField().label("大家好").rowCount(5).vGrow(true));
-            addField(new IntegerSpinnerFormField().label("大小").min(1).max(1000).value(444).editable(true).validation(f -> {
-                if (f.getValue() < 300) {
-                    throw new RuntimeException("大小不能小于300");
-                }
-            }));
-        }
+    public static class FormDialogTestApp extends Application {
 
         @Override
-        protected void okButtonClicked(ActionEvent event) {
-            if (validateFields()) {
-                close();
+        public void start(Stage primaryStage) throws Exception {
+            AppPrimaryStage.setPrimaryStage(primaryStage);
+
+            primaryStage.setScene(new Scene(LayoutBuilder.vbox(50, 0,
+                ButtonBuilder.button("打开对话框", this::openDialog))));
+
+            primaryStage.show();
+        }
+
+        private void openDialog() {
+            MyFormDialog myFormDialog = new MyFormDialog(AppPrimaryStage.getPrimaryStage());
+            myFormDialog.setTitle("测试表单");
+            myFormDialog.setWidth(500);
+            myFormDialog.setHeight(400);
+            myFormDialog.show();
+        }
+
+        //////////////////////////////////////////////////////////////
+
+        public static class MyFormDialog extends FormDialog {
+
+            public MyFormDialog(Stage owner) {
+                super(owner);
+                addField(new TextFormField()
+                    .label("名字").text("1231231")
+                    .validation(f -> {
+                        if (f.getText().isEmpty()) {
+                            throw new RuntimeException("名字不能为空");
+                        }
+                    })
+                );
+                addField(new TextAreaFormField().label("大家好").rowCount(5).vGrow(true));
+                addField(new IntegerSpinnerFormField()
+                    .label("大小").min(1).max(1000).value(444).editable(true)
+                    .validation(f -> {
+                        if (f.getValue() < 300) {
+                            throw new RuntimeException("大小不能小于300");
+                        }
+                    })
+                );
+            }
+
+            @Override
+            protected void okButtonClicked(ActionEvent event) {
+                if (validateFields()) {
+                    close();
+                }
             }
         }
     }
--- src/test/java/com/hyd/fx/notifyicon/NotifyIconTest.java
+++ src/test/java/com/hyd/fx/notifyicon/NotifyIconTest.java
@@ -1,64 +1,64 @@
 package com.hyd.fx.notifyicon;
 
 import com.hyd.fx.dialog.AlertDialog;
-import java.awt.Font;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
-import java.awt.SystemTray;
-import java.awt.TrayIcon;
-import java.awt.image.BufferedImage;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 import javax.imageio.ImageIO;
-import javax.swing.UIManager;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public class NotifyIconTest extends Application {
-
-    private Stage primaryStage;
+public class NotifyIconTest {
 
     public static void main(String[] args) throws Exception {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        launch(args);
+        Application.launch(NotifyIconTestApp.class, args);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
-        primaryStage.setOnShown(event -> onStageShown());
-        primaryStage.setScene(new Scene(new BorderPane(), 300, 200));
-        primaryStage.show();
-    }
+    public static class NotifyIconTestApp extends Application {
 
-    private void onStageShown() {
-        if (!SystemTray.isSupported()) {
-            AlertDialog.error("错误", "系统不支持通知区域图标");
-            return;
+        private Stage primaryStage;
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            this.primaryStage = primaryStage;
+            primaryStage.setOnShown(event -> onStageShown());
+            primaryStage.setScene(new Scene(new BorderPane(), 300, 200));
+            primaryStage.show();
         }
 
-        SystemTray tray = SystemTray.getSystemTray();
+        private void onStageShown() {
+            if (!SystemTray.isSupported()) {
+                AlertDialog.error("错误", "系统不支持通知区域图标");
+                return;
+            }
 
-        try {
-            BufferedImage icon = ImageIO.read(NotifyIconTest.class.getResource("/favorite.png"));
-            TrayIcon trayIcon = new TrayIcon(icon);
-            tray.add(trayIcon);
+            SystemTray tray = SystemTray.getSystemTray();
 
-            final PopupMenu pop = new PopupMenu();
-            MenuItem item1 = new MenuItem(new String("中文".getBytes("GBK")));
-            item1.setFont(Font.getFont("Microsoft Yahei"));
-            item1.addActionListener(e -> {
-                System.out.println(item1.getLabel());
-            });
-            pop.add(item1);
-            pop.add(new MenuItem("主题"));
-            trayIcon.setPopupMenu(pop);
+            try {
+                BufferedImage icon = ImageIO.read(NotifyIconTest.class.getResource("/favorite.png"));
+                TrayIcon trayIcon = new TrayIcon(icon);
+                tray.add(trayIcon);
 
-            primaryStage.setOnCloseRequest(event -> {
-                tray.remove(trayIcon);
-            });
-        } catch (Exception e) {
-            AlertDialog.error("错误", e);
+                final PopupMenu pop = new PopupMenu();
+                MenuItem item1 = new MenuItem(new String("中文".getBytes("GBK")));
+                item1.setFont(Font.getFont("Microsoft Yahei"));
+                item1.addActionListener(e -> {
+                    System.out.println(item1.getLabel());
+                });
+                pop.add(item1);
+                pop.add(new MenuItem("主题"));
+                trayIcon.setPopupMenu(pop);
+
+                primaryStage.setOnCloseRequest(event -> {
+                    tray.remove(trayIcon);
+                });
+            } catch (Exception e) {
+                AlertDialog.error("错误", e);
+            }
         }
     }
 }
